bf9bc625a6b27288c95c96388d1af00badaa7d94
gimhaninavodya <146187436+gimhaninavodya@users.noreply.github.com>
2026-01-16 13:23:15 +0530

    for trending products, use like count logic. for new products, load it with low data usage and more speed. Also, the user profile analysis part limit to sellers.

---
+++ client/src/components/Blogs/blogData.js
@@ -0,0 +1,11 @@
+import blog1 from "../../assets/blogs/goal-setting.png";
+import blog2 from "../../assets/blogs/business-growth.png";
+import blog3 from "../../assets/blogs/remote-work.png";
+import blog4 from "../../assets/blogs/wealth-milestones.png";
+
+export const blogImages = [
+    { id: 1, img: blog1, title: "5 Steps to Achieve Goals" },
+    { id: 2, img: blog2, title: "Business Growth Steps" },
+    { id: 3, img: blog3, title: "Working from Home Tips" },
+    { id: 4, img: blog4, title: "Wealth Development Milestones" },
+];
