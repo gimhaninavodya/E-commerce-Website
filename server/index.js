110ed9046885c0685b26bbd2d1d5ba2536bf3c01
gimhaninavodya <146187436+gimhaninavodya@users.noreply.github.com>
2026-02-02 13:47:10 +0530

    Fix:port

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ app.use("/api/analysis", analysisRoute);
 
 
 // Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}!`);
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}!`);
 });
